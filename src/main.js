f7f21d04aa53b705c47e4ce1ca4042609c9a3745
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2023-02-16 16:53:51 +0100

    2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
 import "./style.css";
 
+// module.exports = {
+//     theme: {
+//         screens: {
+//           sm: '480px',
+//           md: '768px',
+//           lg: '976px',
+//           xl: '1440px',
+//         },
+//     }
+//     theme: {
+//         colors: {
+//             primary: "#5000ca",
+//             accent: "#72f1b8",
+//         },
+//         extend: {
+//             fontFamily: "system-ui", sans-serif”,
+//         },
+//     },
+//   }
